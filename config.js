57877b8dc44a808c1015bcddba3de02104c8493e
Madao088 <hammaduzair786@gmail.com>
2017-09-28 08:50:09 +0530

    final

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     user: 'admin',
     password: 'scherzinger',
     db:'test',
-    s3_accessKeyId: "AKIAJW262TBWRKZKYFSQ",
-    s3_secretAccessKey: "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",
-    s3_region: "ap-south-1"
+    s3_accessKeyId: "",
+    s3_secretAccessKey: "",
+    s3_region: ""
 }
